57f224b7aced33cd7553b7a9cbf7c4d6459abed3
Luca Sartori <luca.sartori@avaloq.com>
2022-07-28 14:55:38 +0200

    added delete button for expense and total epenses div

--- script_1.js
+++ script_1.js
@@ -56,24 +56,64 @@ class Expense {
 // FUNCTIONS
 // ==========================================================================
 function submitNewExpense(){
-	// Retrieve description, amount, currency and category
-	const exp_descr = document.getElementById("exp_descr").value; 
-	const exp_amount = document.getElementById("exp_amount").value; 
-	const exp_currency = document.getElementById('exp_currency').value; 
-	const exp_category = document.getElementById('exp_category').value; 
-
-	// Create a new expense
-	const expense = new Expense(exp_descr, exp_amount, exp_currency, exp_category);
 	
-	// Add it to the list
-	exp_all.push(expense);
+	// Retrieve description, amount, currency and category
+	const exp_descr = document.getElementById("exp_descr"); 
+	const exp_amount = document.getElementById("exp_amount"); 
+	const exp_currency = document.getElementById('exp_currency'); 
+	const exp_category = document.getElementById('exp_category'); 
+	
+	
+	// If amount or descriptor is missing, send alert. otherwise proceed
+	if(exp_descr.value==='' || exp_amount.value===''){
+		alert("Please insert a valid descriptor and a proper amount for this expense");
+	}
+	else {
+	
+		// Create a new expense
+		const expense = new Expense(exp_descr.value, exp_amount.value, exp_currency.value, exp_category.value);
+		
+		// Add it to the list
+		exp_all.push(expense);
+		
+		// Clear the content of the text boxes
+		exp_descr.value='';
+		exp_amount.value='';
+		
+		// Inform User
+		alert("The expense has been recorded");
+		
+		
+		// Update total expenses div
+		updateTotalExpenses();
+		
+		// Show list of all expenses
+		listAllExpenses();
 
+	}
 }
 
+
+// Loop through all the expenses and update the sum on the relative div
+function updateTotalExpenses(){
+	
+	exp_total = 0;
+	for (const exp of exp_all){
+		exp_total += exp.amount_CHF;
+	}
+	
+	// update the text
+	const tot_exp_span = document.getElementById("total_expenses_span");
+	tot_exp_span.textContent = `Total expenses: ${exp_total} CHF`;
+	
+}
+
+
+// Populate the <select> category with all available categories
 function populateCategories(){
 	const selectCat = document.getElementById("exp_category");
 	
-	// First, remove all options >> remove the placeholder
+	// First, remove all options >> remove the placeholder 
 	while (selectCat.options.length > 0) {
         selectCat.remove(0);
     }
@@ -86,6 +126,8 @@ function populateCategories(){
 	
 }
 
+
+// For each category, calculate the partial total amount of epenses
 function categorizeExpenses(){
 	
 	shares = {};
@@ -121,9 +163,10 @@ function drawPieChart(){
 		const canvasDiv = document.createElement('div');
 		const canvas = document.createElement('canvas');
 		
-		canvasDiv.className ="canvas_div";
+		canvasDiv.className ="outer_div";
 		canvasDiv.id ="canvas_div";
-		canvas.id ="canvas";
+		
+		canvas.id ="pie_canvas";
 		
 		canvasDiv.appendChild(canvas);
 		body.appendChild(canvasDiv);
@@ -131,13 +174,13 @@ function drawPieChart(){
 		// Find total expense for each category
 		shares = categorizeExpenses();
 		
-		
-		const width = canvas.width = window.innerWidth;
-		const height = canvas.height = window.innerHeight/1.5;
-		const ctx = canvas.getContext('2d');
+		// Set canvas size >> Same as parent div
+		canvas.width  = 0.95*canvasDiv.clientWidth; //window.innerWidth;
+		canvas.height = 0.95*canvasDiv.clientHeight; //window.innerHeight;
 		
 		// Draw a pie chart
-		const pie = new pieChart(ctx, shares, width/2, height/3, 200);
+		const ctx = canvas.getContext('2d');
+		const pie = new pieChart(ctx, shares, canvas.width/2, canvas.height/2, canvas.width*0.2);
 		
 		pie.draw();
 		
@@ -157,26 +200,50 @@ function listAllExpenses(){
 		toggleAllExpenses = false;
 	}
 	else {
+		
+		// Create container div
 		const allExpDiv = document.createElement('div');
 		allExpDiv.className ="outer_div";
 		allExpDiv.id = "all_expenses";
+		
+		// Create list
 		const allExpList = document.createElement('ul');
 		
-		for (exp of exp_all){
+		for (let i = 0; i < exp_all.length; i++){
+		//for (exp of exp_all){
+			exp = exp_all[i];
+			
+			// Create span, button and list item
 			const span = document.createElement('span');
+			const btn = document.createElement('button');
 			const listItem = document.createElement('li');
 			
+			// Add an eventListener to the button i.o.t. delete the expense
+			btn.classList.add("single_expense_btn");
+			btn.addEventListener('click', ()=> {
+										allExpList.removeChild(listItem);
+										exp_all.splice(i,1);
+										updateTotalExpenses();
+										});
+			
+			// Append span, button to list item
+			listItem.appendChild(btn);
 			listItem.appendChild(span);
 			
-			span.textContent = `[${exp.date_time}] ${exp.description} - ${exp.amount_CHF}CHF - ${exp.category}`;
+			// Update text in span
+			span.textContent = `[${exp.date_time}] ${exp.description} - ${exp.amount_CHF} CHF - ${exp.category}`;
+			
+			// Append list item to list
 			allExpList.appendChild(listItem);
 		}
+		// Append list to div, and div to body
 		allExpDiv.appendChild(allExpList);
 		body.appendChild(allExpDiv);
 		
 		toggleAllExpenses = true;
 	}
 	
+
 }
 
 
@@ -194,17 +261,19 @@ let toggleAllExpenses = false;
 
 
 // Define standard categories and populate the <select> block
-let categories = ['Uncategorized'];
+let categories = ['Home', 'Food'];
 populateCategories();
 
+// ADD EVENT LISTENERS
+//----------------------
 // add an event listener to 'insert new expense' submit button
 const exp_submit_btn = document.querySelector('#exp_submit');
 exp_submit_btn.addEventListener('click', submitNewExpense);
 
-// add an event listener to 'draw pie chart' button
+// add an event listener to 'toggle pie chart' button
 const draw_chart_btn = document.querySelector('#pie_chart_on');
 draw_chart_btn.addEventListener('click', drawPieChart);
 
-// add an event listener to 'all expenses' button
+// add an event listener to 'toggle all expenses' button
 const all_exp_btn = document.querySelector('#all_exp');
 all_exp_btn.addEventListener('click', listAllExpenses);
--- script_2.js
+++ script_2.js
@@ -28,6 +28,14 @@ class pieChart{
 		this.xPos = xPos;
 		this.yPos = yPos;
 		this.radius = radius;
+		
+		// Compute total share >> used to draw pie slices according to percentages
+		this.shares_tot = 0;
+		for (const share of Object.keys(this.shares)){
+			this.shares_tot += shares[share];
+			
+		}
+
 	}
 	
 	
@@ -39,10 +47,23 @@ class pieChart{
 		
 		for (const share of Object.keys(this.shares)) {
 			
+			// Determine share part in %
+			const share_pc = shares[share]/this.shares_tot;
+			
 			// Determine min and max angles for each share. Then, determing the bisect angle >> to place the label
-			const deltaDeg = 360*shares[share];
+			const deltaDeg = 360*share_pc;
 			const maxDeg = minDeg + deltaDeg;
 			const bisectDeg =  minDeg +(maxDeg - minDeg)/2; 
+			
+			// Print for debug
+			console.log('------------------');
+			console.log('share: ' + shares[share]);
+			console.log('shares TOT: ' + this.shares_tot);
+			console.log('shares %: ' + share_pc);
+			console.log('minDeg: ' + minDeg);
+			console.log('maxDeg: ' + maxDeg);
+			console.log('bisectDeg: ' + bisectDeg);
+			console.log('------------------');
 			
 			// Create a color
 			const color = randomRGB();
@@ -55,21 +76,21 @@ class pieChart{
 			this.ctx.fill();
 			
 			//// Draw the label
-			const labelDist = this.radius + 50; 
+			const labelDist = 1.2*this.radius; 
 			
 			const xLabel = this.xPos + labelDist * Math.cos(deg_to_rad(bisectDeg));
 			const yLabel = this.yPos + (labelDist * Math.sin(deg_to_rad(bisectDeg)));
 			const labelSize = 30;
 			
 			
-			this.ctx.font = "40px Verdana";
+			this.ctx.font = "1em Verdana";
 			this.ctx.fillStyle = color;
-			this.ctx.fillText(share + ": " +  this.shares[share], xLabel, yLabel + 40 );
+			this.ctx.fillText(share + ": " +  this.shares[share] + " CHF", xLabel, yLabel );
 			
 			if (bisectDeg <= 180){
-				this.ctx.textAlign = "right";
+				this.ctx.textAlign = "center";
 			} else {
-				this.ctx.textAlign = "left";
+				this.ctx.textAlign = "center";
 			}
 			
 			minDeg = maxDeg;
